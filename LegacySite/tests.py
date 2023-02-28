1092d149ef0d0e62502f608d2a1ca7fd31d266c6
Rana <pranavakumar196@gmail.com>
2023-02-28 13:56:17 -0500

    Setting a testcase for XSS

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -17,3 +17,7 @@ class MyTest(TestCase):
     def test_get_card(self):
         allcards = Card.objects.all()
         self.assertNotEqual(len(allcards), 0)
+   
+    def test_visit_website_no_alert(self):
+        response = self.client.get('http://localhost:8000/buy/1?director=<script>alert("hello")</script>')
+        self.assertEqual(response.status_code, 200)
