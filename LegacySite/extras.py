3875dfb970af03a09510f0872003fe8b1fc8488c
Rana <pranavakumar196@gmail.com>
2023-03-10 22:38:54 -0500

    Fixing Command injection vulnerability - 1

--- LegacySite/extras.py
+++ LegacySite/extras.py
@@ -63,12 +63,12 @@ def parse_card_data(card_file_data, card_path_name):
         return card_file_data
     except (json.JSONDecodeError, UnicodeDecodeError):
         pass
-    with open('Temp/binary', 'wb') as card_file:
+    with open('binary', 'wb') as card_file:
         card_file.write(card_file_data)
     # KG: Are you sure you want the user to control that input?
-    print(f"running: {CARD_PARSER} 2 Temp/binary > Temp/binary;")
-    ret_val = system(f"{CARD_PARSER} 2 Temp/binary > Temp/binary;")
+    print(f"running: {CARD_PARSER} 2 binary > binary;")
+    ret_val = system(f"{CARD_PARSER} 2 binary > binary;")
     if ret_val != 0:
         return card_file_data
-    with open("Temp/binary", 'rb') as tmp_file:
+    with open("binary", 'rb') as tmp_file:
         return tmp_file.read()
