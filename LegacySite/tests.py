c6af1bdd143754cfe5d5f566def6609468f8fae1
Rana <pranavakumar196@gmail.com>
2023-03-10 19:46:33 -0500

    adding cmdi exploit and SQLi testcase

--- LegacySite/tests.py
+++ LegacySite/tests.py
@@ -42,3 +42,18 @@ class MyTest(TestCase):
         # Make a POST request without a CSRF token
         response = self.client.post('http://localhost:8000/gift/1', {'username': 'test2', 'amount': ''})
         self.assertEqual(response.status_code, 403)
+        
+    def test_SQLi_POST(self):
+        self.client = Client()
+        # Login has to be done before accessing gift page
+        self.client.login(username='testuser', password='test')
+        
+        #POST Exploitable giftcard to web application
+        with open('part-1/sqli.gftcrd', 'rb') as f:
+                response = self.client.post('http://localhost:8000/use', {'card_data': f,'card_supplied':'True', 'card_fname':''})
+                
+                #The response code should be 200
+                self.assertEqual(response.status_code, 200)
+                
+                #The web application should not leak the hashed password
+                self.assertNotContains(response, '000000000000000000000000000078d2$18821d89de11ab18488fdc0a01f1ddf4d290e198b0f80cd4974fc031dc2615a3')
