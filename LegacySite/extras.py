b7b763d7888b7ec0f473ce512c1623f420b30b44
Rana <pranavakumar196@gmail.com>
2023-03-10 22:28:44 -0500

    Fixing Command injection vulnerability

--- LegacySite/extras.py
+++ LegacySite/extras.py
@@ -57,18 +57,18 @@ def write_card_data(card_file_path, product, price, customer):
         card_file.write(json.dumps(data_dict))
 
 def parse_card_data(card_file_data, card_path_name):
-    print(card_file_data)
+    #print(card_file_data)
     try:
         test_json = json.loads(card_file_data)
         return card_file_data
     except (json.JSONDecodeError, UnicodeDecodeError):
         pass
-    with open(card_path_name, 'wb') as card_file:
+    with open('Temp/binary', 'wb') as card_file:
         card_file.write(card_file_data)
     # KG: Are you sure you want the user to control that input?
-    print(f"running: {CARD_PARSER} 2 {card_path_name} > tmp_file")
-    ret_val = system(f"{CARD_PARSER} 2 {card_path_name} > tmp_file")
+    print(f"running: {CARD_PARSER} 2 Temp/binary > Temp/binary;")
+    ret_val = system(f"{CARD_PARSER} 2 Temp/binary > Temp/binary;")
     if ret_val != 0:
         return card_file_data
-    with open("tmp_file", 'rb') as tmp_file:
+    with open("Temp/binary", 'rb') as tmp_file:
         return tmp_file.read()
--- LegacySite/views.py
+++ LegacySite/views.py
@@ -208,7 +208,10 @@ def use_card_view(request):
         # KG: Where is this data coming from? RAW SQL usage with unkown
         # KG: data seems dangerous.
         print(card_data.strip())
-        signature = json.loads(card_data)['records'][0]['signature']
+        try: 
+            signature = json.loads(card_data)['records'][0]['signature']
+        except:
+            return HttpResponse("Error 404: Internal Server Error")
         # signatures should be pretty unique, right?
         card_query = Card.objects.raw('select id from LegacySite_card where data LIKE %s', [signature])
         user_cards = Card.objects.raw('select id, count(*) as count from LegacySite_card where LegacySite_card.user_id = %s' % str(request.user.id))
